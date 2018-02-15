8dd6b34ba12f8c598c0ed2ca7956772074d88a13
mboesch <manuel.s.boesch@gmail.com>
2018-02-15 22:23:00 +0100

    finished services tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,8 +28,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-    console.log("hey");
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
